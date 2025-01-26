da633ae7ea8c7c420283bc78f518ad60d66a0484
Adam Tedder <33036071+Watson121@users.noreply.github.com>
2025-01-26 20:34:43 +0000

    Added in Shop Item Data Asset

---
+++ Source/BfbLabsCrossTheRoad/Public/RewardSystem/ShopItem.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "ShopItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BFBLABSCROSSTHEROAD_API UShopItem : public UDataAsset
+{
+	GENERATED_BODY()
+	
+protected:
+
+	UPROPERTY(EditAnywhere, Category = "Shop Item")
+	FText itemName;
+
+	UPROPERTY(EditAnywhere, Category = "Shop Item")
+	FText itemDescription;
+
+	UPROPERTY(EditAnywhere, Category = "Shop Item")
+	class UTexture2D* itemImage;
+
+	UPROPERTY(EditAnywhere, Category = "Shop Item", meta = (ClampMin = 2, ClampMax = 6))
+	int itemCost = 2;
+
+public:
+
+	UFUNCTION(BlueprintPure)
+	FORCEINLINE FText ItemName() const { return itemName; }
+
+	UFUNCTION(BlueprintPure)
+	FORCEINLINE FText ItemDescription() const { return itemDescription; }
+
+	UFUNCTION(BlueprintPure)
+	FORCEINLINE UTexture2D* ItemImage() const { return itemImage; }
+
+	UFUNCTION(BlueprintPure)
+	FORCEINLINE int ItemCost() const { return itemCost; }
+
+};

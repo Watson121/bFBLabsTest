0ed3f124e00411dfe77a65b7ae6dd0f0cc1660cf
Adam Tedder <33036071+Watson121@users.noreply.github.com>
2025-01-27 00:32:34 +0000

    Shop Screen Parent updated to use Game Screen

---
+++ Source/BfbLabsCrossTheRoad/Private/RewardSystem/ShopScreen.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RewardSystem/ShopScreen.h"
+
+void UShopScreen::PurchaseItem()
+{
+}
+
+void UShopScreen::NativeConstruct()
+{
+	Super::NativeConstruct();
+}
